d4ab53ff639a5c1304b9212a3e42cb9b63768fe5
Miroslav Gurnyak <mirmiroslav@gmail.com>
2025-12-19 16:25:38 +0300

    test render

--- script.js
+++ script.js
@@ -8,3 +8,28 @@ function resizeCanvas() {
 
 window.addEventListener('resize', resizeCanvas)
 resizeCanvas()
+
+function draw() {
+  ctx.clearRect(0, 0, canvas.width, canvas.height)
+
+  // canvas center
+  const cx = canvas.width / 2
+  const cy = canvas.height / 2
+
+  //axis
+  ctx.strokeStyle = '#555'
+  ctx.beginPath()
+  ctx.moveTo(0, cy)
+  ctx.lineTo(canvas.width, cy)
+  ctx.moveTo(cx, 0)
+  ctx.lineTo(cx, canvas.height)
+  ctx.stroke()
+
+  // center dot
+  ctx.fillStyle = 'red'
+  ctx.beginPath()
+  ctx.arc(cx, cy, 5, 0, Math.PI * 2)
+  ctx.fill()
+}
+
+draw()
