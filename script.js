b93de1f1cfcc72f587666f5df397a7ceb3678679
Miroslav Gurnyak <mirmiroslav@gmail.com>
2025-12-19 16:15:45 +0300

    canvas stuff

---
+++ script.js
@@ -0,0 +1,10 @@
+const canvas = document.getElementById('canvas')
+const ctx = canvas.getContext('2d')
+
+function resizeCanvas() {
+  canvas.width = canvas.clientWidth
+  canvas.height = canvas.clientHeight
+}
+
+window.addEventListener('resize', resizeCanvas)
+resizeCanvas()
