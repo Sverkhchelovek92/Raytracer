dda8195fbb348e72284d81509a1b7bb3b16810f8
Miroslav Gurnyak <mirmiroslav@gmail.com>
2025-12-23 15:12:09 +0300

    holy light

--- script.js
+++ script.js
@@ -16,12 +16,36 @@ const light = {
   radius: 5,
 }
 
+function drawRays(light) {
+  const maxLength = Math.max(canvas.width, canvas.height)
+  ctx.strokeStyle = 'rgba(255, 255, 200, 0.3)'
+  ctx.lineWidth = 1
+
+  for (let i = 0; i < light.rays; i++) {
+    const angle = (i / light.rays) * Math.PI * 2
+
+    const dx = Math.cos(angle)
+    const dy = Math.sin(angle)
+
+    ctx.beginPath()
+    ctx.moveTo(light.x, light.y)
+    ctx.lineTo(light.x + dx * maxLength, light.y + dy * maxLength)
+    ctx.stroke()
+  }
+}
+
+function drawLight(light) {
+  ctx.fillStyle = 'yellow'
+  ctx.beginPath()
+  ctx.arc(light.x, light.y, light.radius, 0, Math.PI * 2)
+  ctx.fill()
+}
+
 function draw() {
   ctx.clearRect(0, 0, canvas.width, canvas.height)
 
-  // canvas center
-  const cx = canvas.width / 2
-  const cy = canvas.height / 2
+  drawRays(light)
+  drawLight(light)
 }
 
 draw()
