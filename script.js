90e07e5834e977aba5ca8795f0e6b02a68dd63e9
Miroslav Gurnyak <mirmiroslav@gmail.com>
2025-12-23 14:29:59 +0300

    const light

--- script.js
+++ script.js
@@ -9,27 +9,19 @@ function resizeCanvas() {
 window.addEventListener('resize', resizeCanvas)
 resizeCanvas()
 
+const light = {
+  x: canvas.width / 2,
+  y: canvas.height / 2,
+  rays: 180,
+  radius: 5,
+}
+
 function draw() {
   ctx.clearRect(0, 0, canvas.width, canvas.height)
 
   // canvas center
   const cx = canvas.width / 2
   const cy = canvas.height / 2
-
-  //axis
-  ctx.strokeStyle = '#555'
-  ctx.beginPath()
-  ctx.moveTo(0, cy)
-  ctx.lineTo(canvas.width, cy)
-  ctx.moveTo(cx, 0)
-  ctx.lineTo(cx, canvas.height)
-  ctx.stroke()
-
-  // center dot
-  ctx.fillStyle = 'red'
-  ctx.beginPath()
-  ctx.arc(cx, cy, 5, 0, Math.PI * 2)
-  ctx.fill()
 }
 
 draw()
